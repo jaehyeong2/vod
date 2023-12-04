cbefcab9fec7bad4f6303e985a5c6a47de0780d2
jaehyeong <wogud1514@gmail.com>
2023-12-04 22:05:10 +0900

    start

---
+++ src/test/java/jjfactory/vod/VodApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.vod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
